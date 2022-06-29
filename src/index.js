cd66f9973a0bcc8bbeda1520b8e3d30a8e659520
Andrei Milashevich <AndreiMilashevich@gmail.com>
2022-06-29 23:03:38 +0300

    feat: create test yaml

--- src/index.js
+++ src/index.js
@@ -2,14 +2,33 @@ import "./style.scss";
 import printMe from "./print";
 
 const container = document.querySelector(".page_container");
+const buttons =  [...document.querySelectorAll("button")];
 
-greet();
+buttons.forEach(item => addEventListener("click", (event) => {
+  let pageData = pages.filter(item => event.target.id === item.id);
+  window.history.pushState({}, pageData[0].route, window.location.origin + pageData[0].route);
+  render(pageData);
+  console.log(document.location);
+}));
 
-function greet() {
-  container.innerHTML = "hello webpack";
-  printMe();
+const pages = [
+  {id: "main", text: "This is main page", route: "/"},
+  {id: "portfolio", text: "This is portfolio page", route: "/portfolio"},
+  {id: "about", text: "This is about page", route: "/about"},
+];
+
+init();
+
+function render(pageData) {
+  container.innerHTML = pageData[0].text;
 }
 
+function init() {
+  render(pages[0]);
+  let pathname = document.location.pathname;
+  let pageData = pages.filter(item => pathname === item.route);
+  render(pageData);
+}
 
 
 
